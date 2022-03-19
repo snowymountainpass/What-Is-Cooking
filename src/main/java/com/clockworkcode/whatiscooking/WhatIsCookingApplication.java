2b1970fdcd7544b0cdeab4e5adb9d8052aa566c6
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2022-03-19 14:11:07 +0200

    Initial commit

---
+++ src/main/java/com/clockworkcode/whatiscooking/WhatIsCookingApplication.java
@@ -0,0 +1,13 @@
+package com.clockworkcode.whatiscooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatIsCookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatIsCookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clockworkcode/whatiscooking/WhatIsCookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clockworkcode.whatiscooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatIsCookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
